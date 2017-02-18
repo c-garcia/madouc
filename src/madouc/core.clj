91f03d71738e6dc06e4f9aac4b4af0990916b836
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2017-02-18 19:57:47 +0100

    Server logs when starting

--- src/madouc/core.clj
+++ src/madouc/core.clj
@@ -1,8 +1,11 @@
 (ns madouc.core
   (:require [immutant.web :as server]
-            [madouc.handlers :as app])
+            [madouc.handlers :as app]
+            [environ.core :refer [env]]
+            [taoensso.timbre :as timbre])
   (:gen-class))
 
 (defn -main 
   [& args]
+  (timbre/info "Starting server...")
   (server/run app/entrypoint))
