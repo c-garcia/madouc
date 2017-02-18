58c895061b3deb15c60d931a9039b7e7c0b76dae
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2017-02-18 19:42:54 +0100

    Enable logging and server logging via timbre

--- project.clj
+++ project.clj
@@ -5,6 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.5.1"]
-                 [org.immutant/web "2.1.6"]]
+                 [com.taoensso/timbre "4.8.0"]
+                 [com.fzakaria/slf4j-timbre "0.3.4"]
+                 [org.immutant/web "2.1.6"
+                  :exclusions [ch.qos.logback/logback-classic]]]
   :main madouc.core
   :profiles {:uberjar {:aot :all}})
