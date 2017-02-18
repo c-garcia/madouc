3a29b9b9878b21b2adf3a6141590ae7cdca7366f
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2017-02-18 19:28:26 +0100

    Add immutant server

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.5.1"]
-                 [org.immutant/web "2.1.6"]])
+                 [org.immutant/web "2.1.6"]]
+  :main madouc.core
+  :profiles {:uberjar {:aot :all}})
