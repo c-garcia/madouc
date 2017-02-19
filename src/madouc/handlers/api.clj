e1d73a47abf97ef0a78964831a06812c982b7237
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2017-02-19 10:35:43 +0100

    Configure swagger

--- src/madouc/handlers/api.clj
+++ src/madouc/handlers/api.clj
@@ -2,6 +2,19 @@
   (:require [ring.util.http-response :as http-resp]
             [compojure.api.sweet :refer :all]))
 
-(defapi handler
+(defapi handler-old
   (context "/api" []
-           (GET "/status" [] (http-resp/ok {:api-status "ok"}))))
+    (GET "/status" [] (http-resp/ok {:api-status "ok"}))))
+
+
+(def handler
+  (api
+   {:swagger {:ui "/api-docs/"
+              :spec "/api/swagger.json"
+              :data {:info {:title "Madouc API"
+                            :description "API for the Madouc service"
+                            :version "0.1.3"}}}}
+   (context "/api" []
+    (GET "/status" [] (http-resp/ok {:api-status "ok"})))))
+            
+   
