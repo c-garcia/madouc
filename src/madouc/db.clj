f858d0527252803efeb054f7d3e3eb9c76f5852e
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2017-02-23 21:04:47 +0100

    Add db connection and configuration

---
+++ src/madouc/db.clj
@@ -0,0 +1,20 @@
+(ns madouc.db
+  (:require [mount.core :refer [defstate]]
+            [conman.core :as conman]
+            [madouc.config :refer [env]]))
+
+(defn- make-pool-spec
+  [host db user pass]
+  {:jdbc-url (format "jdbc:postgresql://%s/%s" host db)
+   :username user
+   :password pass})
+
+(defstate con
+  :start (conman/connect! (make-pool-spec
+                           (env :db-host)
+                           (env :db-name)
+                           (env :db-user)
+                           (env :db-password)))
+  :stop (conman/disconnect! con))
+  
+
