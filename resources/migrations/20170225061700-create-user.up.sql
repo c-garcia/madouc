cebd883c9a715974f57f86a914487b3ea662d565
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2017-02-25 06:53:02 +0100

    Add migrator tools and create user migrations

---
+++ resources/migrations/20170225061700-create-user.up.sql
@@ -0,0 +1,6 @@
+create table users (
+  id serial,
+  username varchar(80),
+  password char(64)
+)
+  
---
+++ src/madouc/db/migrator.clj
@@ -0,0 +1,30 @@
+(ns madouc.db.migrator
+  (:require [migratus.core :as migratus]
+            [madouc.config :refer [env]]))
+
+
+(defn- config->migratus-params
+  [config]
+  {:store :database
+   :migration-dir "migrations/"
+   :db {:classname "org.postgresql.Driver"
+        :subprotocol "postgres"
+        :subname (format "//%s/%s" (:db-host config) (:db-name config))
+        :user (:db-user config)
+        :password (:db-password config)}})
+  
+(defn migrate!
+  []
+  (migratus/migrate (config->migratus-params env)))
+
+(defn rollback!
+  []
+  (migratus/rollback (config->migratus-params env)))
+
+(defn pending-list
+  []
+  (migratus/pending-list (config->migratus-params env)))
+
+(defn debug-config
+  []
+  (config->migratus-params env))
