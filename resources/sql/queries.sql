34a27d82e046ea63153bf21c99a5a606531c03f4
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2017-02-26 06:57:50 +0100

    Rename db query to validate user

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,3 +1,11 @@
--- :name find-all-users :? :*
+-- :name find-users :? :*
 
 select * from users
+
+-- :name count-users-with-credentials :? :1
+-- :doc finds a user with :username and :password
+
+select count(*) from users
+where username = :username and password = :password
+
+
