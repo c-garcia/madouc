65062fb2ef4ccd37be0b78d5a41175b0d905fbd7
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2017-02-26 06:59:17 +0100

    Add login operation to API

--- src/madouc/handlers/api.clj
+++ src/madouc/handlers/api.clj
@@ -1,12 +1,17 @@
 (ns madouc.handlers.api
   (:require [ring.util.http-response :as http-resp]
             [compojure.api.sweet :refer :all]
-            [madouc.db :as db]))
+            [schema.core :as s]
+            [madouc.db :as db]
+            [madouc.controllers :refer :all]))
 
 (defapi handler-old
   (context "/api" []
     (GET "/status" [] (http-resp/ok {:api-status "ok"}))))
 
+(s/defschema LoginReq {:username String :password String})
+(s/defschema Token {:token String})
+(s/defschema APIError {:error String})
 
 (def handler
   (api
@@ -16,6 +21,11 @@
                             :description "API for the Madouc service"
                             :version "0.1.3"}}}}
    (context "/api" []
-    (GET "/status" [] (http-resp/ok {:api-status "ok"})))))
+     (GET "/status" [] (http-resp/ok {:api-status "ok"}))
+     (POST "/login" []
+       :responses {401 {:schema APIError :description "Unauthorized"}}
+       :return Token
+       :body [req LoginReq]
+       (login-controller req)))))
             
    
