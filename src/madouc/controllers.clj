eb90183f104095a261e52593a1d3f9a88658ec4f
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2017-02-27 01:18:51 +0100

    Add events controller

--- src/madouc/controllers.clj
+++ src/madouc/controllers.clj
@@ -4,6 +4,7 @@
             [madouc.services.login :refer [valid-user?]]
             [madouc.services.token :as token]
             [madouc.services.user :as user]
+            [madouc.services.events :as events]
             [clj-time.core :as t]))
 
 (defn login-controller
@@ -20,5 +21,15 @@
     (if username
       (http-resp/ok (user/get-info username))
       (http-resp/unauthorized {:error "Unauthorized"}))))
-    
 
+(defn events-controller
+  [req]
+  (let [username (get-in req [:identity :sub])]
+    (if username
+      (try
+        (http-resp/ok (events/get-batch username))
+        (catch Exception e
+          (case (-> (ex-data e) :reason)
+            :no-token (http-resp/not-found "Token not found")
+            (http-resp/internal-server-error (.getMessage e)))))
+      (http-resp/unauthorized {:error "Unauthorized"}))))
